9cba370efa352763580ff22ba8d1bb494d8bd094
Seonggoo Kang <ksung91889@naver.com>
2023-05-13 14:14:31 +0900

    vanilla로 redux사용해보기 끝

--- src/index.js
+++ src/index.js
@@ -4,83 +4,82 @@ const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
-const createTodo = toDo => {
-  const li = document.createElement("li");
-  li.innerText = toDo;
-  ul.appendChild(li);
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const addToDo = (text) => {
+  return {
+    type: ADD_TODO,
+    text
+  }
 }
+
+const deleteToDo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id
+  }
+}
+
+const reducer = (state = [], action) => {
+  console.log(action);
+  switch (action.type) {
+    case ADD_TODO:
+      // old state를 return 하면 안됨
+      return [ { text : action.text, id: Date.now() }, ...state]; // 중요! state를 변형시키지 않는다. 새로운 걸 만들어 return 한다
+    case DELETE_TODO:
+      return state.filter(toDo => toDo.id !== action.id ); // 새로운 배열을 뱉어준다
+      // toDo.id -->> 각각의 toDo의 id
+      // action.id -->> 방금 너가 누른 id
+    default:
+      return state;
+  }
+}
+
+// const createTodo = toDo => {
+//   const li = document.createElement("li");
+//   li.innerText = toDo;
+//   ul.appendChild(li);
+// }
+// 이거 대신에 dispatch 할거임
+
+const store = createStore(reducer);
+// store를 수정할 수 있는 유일한 방법 : action
+
+store.subscribe(() => console.log(store.getState()));
+ 
+const dispatchAddToDo = text => {
+  if(text !== "") store.dispatch(addToDo(text)); // type 외에 뭐든 넣을 수 있다
+}
+
+const dispatchDeleteToDo = (e) => {
+  const id = parseInt(e.target.parentNode.id); // html로부터 오는 ID는 string일거싱기에 parseInt해준다.
+  store.dispatch(deleteToDo(id)) // dispatch의 파라미터는 reducer의 액션의 인자로감
+  console.log(e.target.parentNode.id);
+}
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach(toDo => {
+    const li = document.createElement("li");
+    const btn = document.createElement('button');
+    btn.innerText = 'Del';
+    btn.addEventListener('click', dispatchDeleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    ul.appendChild(li);
+    li.appendChild(btn);
+  })
+}
+
+store.subscribe(paintToDos);
 
 const onSubmit = e => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  createTodo(toDo);
+  dispatchAddToDo(toDo);
 }
 
 form.addEventListener('submit', onSubmit);
-
-const add = document.getElementById('add');
-const minus = document.getElementById('minus');
-const number = document.querySelector('span');
-number.innerText = 0;
-// countModifier가 return 하는 것은 data가 된다.
-// 내가 전송한 메세지는 action에 들어감.
-// data modify는 이 안에서만 일어남. 이 reducer 안에서
-
-// 상수화 시킴으로써 오타로 인한 에러를 없애준다.
-const ADD = "ADD";
-const MINUS = "MINUS"; 
-
-// countmodifier가 데이터를 수정하고 이함수가 리턴하는건 이 에플리케이션의 스테이트가 됨
-const countModifier = (count = 0, action) => { // 여기서만 data 수정이 일어남
-  switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
-    default:
-      return count;
-  }
-
-  // if (action.type === "ADD") {
-  //   return count + 1;
-  // } else if(action.type === "MINUS") {
-  //   return count - 1;
-  // } else {
-  //   return count;
-  // }
-};
-
-const countStore = createStore(countModifier);
- 
-const onChange = () => {
-  number.innerText = countStore.getState();
-}
-
-countStore.subscribe(onChange); // If you wanna listen for changes your store.
-
-const handleAdd = () => {
-  countStore.dispatch({type : ADD}) // 이 액션은 객체여야만함. type을 항상 가지고 있음.
-}
-const handleMinus = () => {
-  countStore.dispatch({type : MINUS}) // dispaatch를 이용해서 액션을 reducer에 보냄
-}
-
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
-const num = parseInt(10).toString(2);
-console.log(parseInt(10).toString(2));
-
-console.log(num.split('')); // 배열화
-
-
-// countModifier가 소통하는 법 : countModifier에게 action을 보내
-// message를 send 하는 방법 : dispatch
-// countStore.dispatch({type : "ADD"});
-// countStore.dispatch({type : "ADD"});
-// countStore.dispatch({type : "ADD"});
-// countStore.dispatch({type : "ADD"});
-// countStore.dispatch({type : "ADD"});
-// countStore.dispatch({type : "MINUS"});
-
-// console.log(countStore.getState());
